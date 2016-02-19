f1f8a747eacd8c087ea6b2c6fa0aae3c1210c3e6
Ethan S <nerfsniper@att.net>
2016-02-18 21:21:05 -0600

    Committing some crap I forgot to earlier in Team.cs

--- MyScout/MyScout/src/Classes/Team.cs
+++ MyScout/MyScout/src/Classes/Team.cs
@@ -14,15 +14,62 @@ namespace MyScout
         /// <summary>
         /// The name of the team (E.G. "Channel Cats").
         /// </summary>
-        public string name = "NO NAME LOADED"; //Helpfulness Improved
+        public string name = "NO NAME LOADED"; //Helpfulness improved at the cost of comedy :P
         /// <summary>
         /// The id of the team (E.G. "1094").
         /// </summary>
         public int id = 0000; //Helpfulness Improved
         /// <summary>
-        /// The team's current score.
+        /// The team's total score.
         /// </summary>
-        public int score = 0;
+        public int totalScore = 0;
+        /// <summary>
+        /// The team's total count of defenses crossed.
+        /// </summary>
+        public int teleDefensesCrossedCount = 0;
+        public int teleHighGoals = 0;
+        public int teleLowGoals = 0;
+        public int towerScaledCount = 0;
+
+        /// <summary>
+        /// A list of defenses this team can cross.
+        /// Each index references a different defense:
+        /// [0]: Portcullis
+        /// [1]: Cheval de Frise
+        /// [2]: Moat
+        /// [3]: Ramparts
+        /// [4]: Rock Wall
+        /// [5]: Rough Terrain
+        /// [6]: Sally Port
+        /// [7]: Low Bar
+        /// </summary>
+        public bool[] defensesCrossable = new bool[8];
+
+        /// <summary>
+        /// A score based on how well the team can cross defenses
+        /// </summary>
+        public int crossingPowerScore = 0;
+
+        /// <summary>
+        /// A list of defenses this team has interacted with in auto mode.
+        /// Each index references a different defense:
+        /// [0]: Portcullis
+        /// [1]: Cheval de Frise
+        /// [2]: Moat
+        /// [3]: Ramparts
+        /// [4]: Rock Wall
+        /// [5]: Rough Terrain
+        /// [6]: Sally Port
+        /// [7]: Low Bar
+        /// </summary>
+        public int autoDefensesCrossed = 0;
+        public int autoHighGoals = 0;
+        public int autoLowGoals = 0;
+
+        /// <summary>
+        /// Total amount of times the robot stopped working at any moment on the field
+        /// </summary>
+        public int deathCount = 0;
 
         public Team(int id, string name) { this.id = id; this.name = name; }
     }
