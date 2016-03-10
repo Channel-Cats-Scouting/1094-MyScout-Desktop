f6380d1cb0a57e00e0c6402142f2a32db5ddb911
Radfordhound <radfordhound@users.noreply.github.com>
2016-03-10 03:35:24 -0600

    Last-minute additions before the first release.

--- MyScout/MyScout/Properties/AssemblyInfo.cs
+++ MyScout/MyScout/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyScout")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A potent combination of database, schedule, and score chart that allows for keeping records of each robot's performance for an entire robotics competition.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyScout")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #1094 MyScout
 Created for FRC Competitions
 
-Graham's work-in-progress complete 2016 re-creation of "MyScout."S
+This is the 2016 version of MyScout; a potent combination of database, schedule, and score chart that allows for keeping records of each robot's performance for an entire robotics competition.
